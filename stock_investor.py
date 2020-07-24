44d5dc392c60d4e560023b62982dff4374e5a564
남인 <south-man@daum.net>
2020-07-25 02:51:49 +0900

    수정

--- stock_investor.py
+++ stock_investor.py
@@ -440,7 +440,7 @@ class StockInvestor:
         return now_price, now_cnt
 
     def trade_take_profit_basic(self, now_price, now_cnt, bought_price, high, stop_loss_ratio=10, take_profit_ratio=29,
-                                stop_loss_gab_ratio=29, sell_ratio=50, **params):
+                                stop_loss_gab_ratio=30, sell_ratio=50, **params):
         take_ratio = stop_loss_gab_ratio - stop_loss_ratio
         if now_cnt > 0 and 2 < take_ratio < take_profit_ratio and now_price < self.get_buy_price_with_tax(bought_price):
             take_profit = bought_price * (1 + take_ratio / 100)
